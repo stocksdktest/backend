3df9051bfcb5334cebc9c30994fc80287db865a6
TcPaulyue <1195882464@qq.com>
2020-02-19 10:40:01 +0800

    update

--- README.md
+++ README.md
@@ -1,7 +1,8 @@
 ### 操作说明
+docker-compose.yml中是一个mongodb，`docker-compose up -d`来运行这个容器；
 
-backend目录中是一个为mongodb设计的通用的CRUD服务；
+backend目录中是一个为mongodb设计的通用的CRUD服务；可以直接启动backend.jar来运行。
 
 stocksdktestgateway目录是一个gateway服务，用来接受前端发来的请求，并处理请求格式 使其适应backend的接口，再将处理过的请求转发给backend。
+里面以`sdkVersion`为例，实现了CURD方法，需要参考其实现，对应地更改函数中的`collectionName`.
 
-运行需要docker容器，先在backend目录下`docker-compose up -d`,然后分别启动两个服务。
--- stocksdktestgateway/src/main/java/com/stocktest/gateway/SdkVersionGatewayController.java
+++ stocksdktestgateway/src/main/java/com/stocktest/gateway/SdkVersionGatewayController.java
@@ -17,13 +17,16 @@ import java.util.Map;
 public class SdkVersionGatewayController {
 
     private static final Logger logger = LoggerFactory.getLogger(SdkVersionGatewayController.class);
+
     @PostMapping("/new")
     public JSONObject createSdkVersion(@RequestBody JSONObject params){
         logger.info(params.toString());
-        params.put("title","sdkVersion");
-        RestTemplate restTemplate = new RestTemplate();
-        String uri = "http://localhost:8088/api/documents/new";
-        ResponseEntity<JSONObject> response = restTemplate.postForEntity(uri,params,JSONObject.class);
+
+        params.put("title","sdkVersion"); //title表示collectionName
+        RestTemplate restTemplate = new RestTemplate();//新建一个restTemplate对象
+        String uri = "http://localhost:8088/api/documents/new"; //这是backend的createNewDocument对应的uri，不用更改
+        ResponseEntity<JSONObject> response = restTemplate.postForEntity(uri,params,JSONObject.class); //在sdkVersion的collection中新建一个document
+
         logger.info(response.getBody().toString());
         return response.getBody();
     }
@@ -43,9 +46,11 @@ public class SdkVersionGatewayController {
     public JSONObject getSdkVersionById(@PathVariable String id){
         RestTemplate restTemplate = new RestTemplate();
         String uri = "http://localhost:8088/api/documents/{id}?collectionName={collectionName}";
+
         Map<String, Object> params = new HashMap<>();
         params.put("id",id);
         params.put("collectionName", "sdkVersion");
+
         ResponseEntity<JSONObject> response = restTemplate.getForEntity(uri, JSONObject.class,params);
         logger.info(response.getBody().toString());
         return response.getBody();
@@ -55,14 +60,18 @@ public class SdkVersionGatewayController {
     public JSONObject updateDocumentById(@PathVariable String id
             , @RequestBody JSONObject params) {
         RestTemplate restTemplate = new RestTemplate();
-        String uri = "http://localhost:8088/api/documents/{id}?collectionName={collectionName}";
+        String uri = "http://localhost:8088/api/documents/{id}?collectionName={collectionName}"; //update方法对应的uri，不用更改
+
         Map<String, String> pathParam = new HashMap<>();
         pathParam.put("id", id);
-        pathParam.put("collectionName","sdkVersion");
+        pathParam.put("collectionName","sdkVersion"); //只需考虑这行，将collectionName更改即可
+
+        //http头信息，不用更改
         HttpHeaders headers = new HttpHeaders();
         headers.setContentType(MediaType.APPLICATION_JSON);
         HttpEntity<JSONObject> requestUpdate = new HttpEntity<>(params, headers);
         ResponseEntity<JSONObject> response = restTemplate.exchange(uri, HttpMethod.PUT, requestUpdate, JSONObject.class,pathParam);
+
         logger.info(response.getBody().toString());
         return response.getBody();
     }
@@ -70,10 +79,13 @@ public class SdkVersionGatewayController {
     @DeleteMapping("/{id}")
     public JSONArray deleteDocumentById(@PathVariable String id){
         RestTemplate restTemplate = new RestTemplate();
-        String uri = "http://localhost:8088/api/documents/{id}?collectionName={collectionName}";
+        String uri = "http://localhost:8088/api/documents/{id}?collectionName={collectionName}";//delete对应的uri，不用更改
+
         Map<String, Object> params = new HashMap<>();
         params.put("id",id);
         params.put("collectionName", "sdkVersion");
+
+
         HttpHeaders headers = new HttpHeaders();
         headers.setContentType(MediaType.APPLICATION_JSON);
         HttpEntity<String> requestUpdate = new HttpEntity<>(id,headers);
