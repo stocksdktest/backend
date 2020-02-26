b677c9d9258ca5912ef046a6f9dc5e4bdf59e54b
HonglinXie <41292788+HonglinXie@users.noreply.github.com>
2020-02-26 13:06:31 +0800

    stockTypeCode
    2020-02-26

---
+++ stocksdktestgateway/src/main/java/com/stocktest/gateway/StockTypeCodeGatewayController.java
@@ -0,0 +1,85 @@
+package com.stocktest.gateway;
+
+import com.alibaba.fastjson.JSONArray;
+import com.alibaba.fastjson.JSONObject;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.*;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController
+@CrossOrigin
+@RequestMapping("/api/stocktypecode")
+public class StockTypeCodeGatewayController {
+
+    private static final Logger logger = LoggerFactory.getLogger(StockTypeCodeGatewayController.class);
+    @PostMapping("/new")
+    public JSONObject createStockTypeCode(@RequestBody JSONObject params){
+        logger.info(params.toString());
+        params.put("title","stockTypeCode");
+        RestTemplate restTemplate = new RestTemplate();
+        String uri = "http://localhost:8088/api/documents/new";
+        ResponseEntity<JSONObject> response = restTemplate.postForEntity(uri,params,JSONObject.class);
+        logger.info(response.getBody().toString());
+        return response.getBody();
+    }
+
+    @GetMapping()
+    public JSONArray getStockTypeCode(){
+        RestTemplate restTemplate = new RestTemplate();
+        String uri = "http://localhost:8088/api/documents/?collectionName={collectionName}";
+        Map<String, Object> params = new HashMap<>();
+        params.put("collectionName", "stockTypeCode");
+        ResponseEntity<JSONArray> response = restTemplate.getForEntity(uri, JSONArray.class,params);
+        logger.info(response.getBody().toString());
+        return response.getBody();
+    }
+
+    @GetMapping("/{id}")
+    public JSONObject getSdkVersionById(@PathVariable String id){
+        RestTemplate restTemplate = new RestTemplate();
+        String uri = "http://localhost:8088/api/documents/{id}?collectionName={collectionName}";
+        Map<String, Object> params = new HashMap<>();
+        params.put("id",id);
+        params.put("collectionName", "stockTypeCode");
+        ResponseEntity<JSONObject> response = restTemplate.getForEntity(uri, JSONObject.class,params);
+        logger.info(response.getBody().toString());
+        return response.getBody();
+    }
+
+    @PutMapping("/{id}")
+    public JSONObject updateDocumentById(@PathVariable String id
+            , @RequestBody JSONObject params) {
+        RestTemplate restTemplate = new RestTemplate();
+        String uri = "http://localhost:8088/api/documents/{id}?collectionName={collectionName}";
+        Map<String, String> pathParam = new HashMap<>();
+        pathParam.put("id", id);
+        pathParam.put("collectionName","stockTypeCode");
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_JSON);
+        HttpEntity<JSONObject> requestUpdate = new HttpEntity<>(params, headers);
+        ResponseEntity<JSONObject> response = restTemplate.exchange(uri, HttpMethod.PUT, requestUpdate, JSONObject.class,pathParam);
+        logger.info(response.getBody().toString());
+        return response.getBody();
+    }
+
+    @DeleteMapping("/{id}")
+    public JSONArray deleteDocumentById(@PathVariable String id){
+        RestTemplate restTemplate = new RestTemplate();
+        String uri = "http://localhost:8088/api/documents/{id}?collectionName={collectionName}";
+        Map<String, Object> params = new HashMap<>();
+        params.put("id",id);
+        params.put("collectionName", "stockTypeCode");
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_JSON);
+        HttpEntity<String> requestUpdate = new HttpEntity<>(id,headers);
+        ResponseEntity<JSONArray> response = restTemplate.exchange(uri,HttpMethod.DELETE,requestUpdate,JSONArray.class,params);
+        logger.info(response.getBody().toString());
+        return response.getBody();
+    }
+
+}
