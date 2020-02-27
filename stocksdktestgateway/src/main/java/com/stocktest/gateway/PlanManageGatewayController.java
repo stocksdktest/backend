8426389aa2e8b1d9e230154e0eb4a8315247e388
HonglinXie <41292788+HonglinXie@users.noreply.github.com>
2020-02-27 15:42:14 +0800

    PlanManage
    2020-02-27

---
+++ stocksdktestgateway/src/main/java/com/stocktest/gateway/PlanManageGatewayController.java
@@ -0,0 +1,96 @@
+package com.stocktest.gateway;
+
+import com.alibaba.fastjson.JSONArray;
+import com.alibaba.fastjson.JSONObject;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.*;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController
+@CrossOrigin
+@RequestMapping("/api/planmanage")
+public class PlanManageGatewayController {
+
+    private static final Logger logger = LoggerFactory.getLogger(PlanManageGatewayController.class);
+
+    @PostMapping("/new")
+    public JSONObject createPlanManage(@RequestBody JSONObject params){
+        logger.info(params.toString());
+        params.put("title","planManage"); //title表示collectionName
+        RestTemplate restTemplate = new RestTemplate();//新建一个restTemplate对象
+        String uri = "http://localhost:8088/api/documents/new"; //这是backend的createNewDocument对应的uri，不用更改
+        ResponseEntity<JSONObject> response = restTemplate.postForEntity(uri,params,JSONObject.class); //在sdkVersion的collection中新建一个document
+
+        logger.info(response.getBody().toString());
+        return response.getBody();
+    }
+
+    @GetMapping()
+    public JSONArray getPlanManage(){
+        RestTemplate restTemplate = new RestTemplate();
+        String uri = "http://localhost:8088/api/documents/?collectionName={collectionName}";
+        Map<String, Object> params = new HashMap<>();
+        params.put("collectionName", "planManage");
+        ResponseEntity<JSONArray> response = restTemplate.getForEntity(uri, JSONArray.class,params);
+        logger.info(response.getBody().toString());
+        return response.getBody();
+    }
+
+    @GetMapping("/{id}")
+    public JSONObject getPlanManageById(@PathVariable String id){
+        RestTemplate restTemplate = new RestTemplate();
+        String uri = "http://localhost:8088/api/documents/{id}?collectionName={collectionName}";
+
+        Map<String, Object> params = new HashMap<>();
+        params.put("id",id);
+        params.put("collectionName", "planManage");
+
+        ResponseEntity<JSONObject> response = restTemplate.getForEntity(uri, JSONObject.class,params);
+        logger.info(response.getBody().toString());
+        return response.getBody();
+    }
+
+    @PutMapping("/{id}")
+    public JSONObject updateDocumentById(@PathVariable String id
+            , @RequestBody JSONObject params) {
+        RestTemplate restTemplate = new RestTemplate();
+        String uri = "http://localhost:8088/api/documents/{id}?collectionName={collectionName}"; //update方法对应的uri，不用更改
+
+        Map<String, String> pathParam = new HashMap<>();
+        pathParam.put("id", id);
+        pathParam.put("collectionName","planManage"); //只需考虑这行，将collectionName更改即可
+
+        //http头信息，不用更改
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_JSON);
+        HttpEntity<JSONObject> requestUpdate = new HttpEntity<>(params, headers);
+        ResponseEntity<JSONObject> response = restTemplate.exchange(uri, HttpMethod.PUT, requestUpdate, JSONObject.class,pathParam);
+
+        logger.info(response.getBody().toString());
+        return response.getBody();
+    }
+
+    @DeleteMapping("/{id}")
+    public JSONArray deleteDocumentById(@PathVariable String id){
+        RestTemplate restTemplate = new RestTemplate();
+        String uri = "http://localhost:8088/api/documents/{id}?collectionName={collectionName}";//delete对应的uri，不用更改
+
+        Map<String, Object> params = new HashMap<>();
+        params.put("id",id);
+        params.put("collectionName", "planManage");
+
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_JSON);
+        HttpEntity<String> requestUpdate = new HttpEntity<>(id,headers);
+        ResponseEntity<JSONArray> response = restTemplate.exchange(uri,HttpMethod.DELETE,requestUpdate,JSONArray.class,params);
+        logger.info(response.getBody().toString());
+        return response.getBody();
+    }
+
+}
